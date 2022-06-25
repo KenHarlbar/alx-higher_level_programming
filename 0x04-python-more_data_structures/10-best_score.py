eb752b10c1df2b0d89794565810bfcfb18a9384f
KenHarlbar <balotech514@gmail.com>
2022-06-25 09:06:00 +0100

    Hot fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        list_of_names = list(a_dictionary.keys())
-        max_score = a_dictionary[list_of_names[0]]
-        for i in list_of_names:
-            if a_dictionary[i]:
-                if a_dictionary[i] > max_score:
-                    max_score = a_dictionary[i]
-                    best_student = i
-            else:
-                continue
-    else:
-        return None
-    return best_student
+        return max(a_dictionary, key=a_dictionary.get)
+    return None

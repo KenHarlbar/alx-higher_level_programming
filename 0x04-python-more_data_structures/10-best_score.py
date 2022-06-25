9b977362cc0dbac48d1c51392012fcc88a14626a
KenHarlbar <balotech514@gmail.com>
2022-06-25 08:52:02 +0100

    Return a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        list_of_names = list(a_dictionary.keys())
+        max_score = a_dictionary[list_of_names[0]]
+        for i in list_of_names:
+            if a_dictionary[i] > max_score:
+                max_score = a_dictionary[i]
+                best_student = i
+    else:
+        return None
+    return best_student

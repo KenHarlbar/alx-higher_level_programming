c6db5604ba9110b92318192872d22bf08d5a1824
KenHarlbar <balotech514@gmail.com>
2022-06-28 16:00:54 +0100

    Hot fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 class Square:
+    """Class Square that defines a square with \
+            a private attribute 'size'
+    """
     def __init__(self, size):
+        """Initialize method that stores the size \
+                of the square
+
+        Args:
+            param1 (int): size of the square
+        """
         self.__size = size

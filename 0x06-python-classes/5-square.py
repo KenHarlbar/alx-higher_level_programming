f1283dfdf1ca16eb62f57aa661ea3c6d2d485887
KenHarlbar <balotech514@gmail.com>
2022-06-28 17:44:59 +0100

    Write a class Square that defines a square by size

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""module for class square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize method for size of square
+
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Method for retrieving size of square
+
+        Returns:
+            size
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method for setting size of square"""
+
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        """Method for computing area of square
+
+        Returns:
+            area
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """Method to display the square"""
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end='')
+            print()

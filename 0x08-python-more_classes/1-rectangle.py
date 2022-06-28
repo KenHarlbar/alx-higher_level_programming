ad6c4989718de3970a400a5fccf2921437d160a3
KenHarlbar <balotech514@gmail.com>
2022-06-28 21:58:23 +0100

    Hot fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,6 +8,10 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize method for width and height \
             of rectangle
+
+        Args:
+            width: width of rectangle
+            height: height of rectangle
         """
 
         self.__width = width
@@ -35,9 +39,13 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Method to set width"""
+        """Method to set width
 
-        if type(value) != int:
+        Args:
+            width - width of rectangle
+        """
+
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
@@ -46,9 +54,13 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Method to set height"""
+        """Method to set height
 
-        if type(value) != int:
+        Args:
+            height - height of rectangle
+        """
+
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value < 0:
             raise ValueError('height must be >= 0')

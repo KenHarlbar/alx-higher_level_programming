982c81819cc9835a2c3a94f3dfcf2d19010fdecc
baaioy <balotech514@gmail.com>
2022-07-06 03:33:39 +0100

    Define a Student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" Class module for Student """
+
+
+class Student:
+    """ Student class that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes Student
+
+        Args:
+            first_name: Student first name
+            last_name: Student last name
+            age: Student age
+
+        Returns:
+            nothing
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=''):
+        """ Function that retrieves a dictionary representation
+        of a Student instance
+
+        Args:
+            attr: key(s)
+
+        Returns:
+            dictionary representation of a Student Instance
+        """
+        dico = self.__dict__.copy()
+
+        if type(attr) == list:
+            for each in attr:
+                if type(each) != str:
+                    return dico
+
+            new_dict = {}
+
+            for each in attr:
+                try:
+                    new_dict[each] = dico[each]
+                except Exception:
+                    continue
+            return new_dict
+
+        return dico
+
+    def reload_from_json(self, json):
+        """ Method that replaces all attributes of the Student
+        instance
+
+        Args:
+            json: json file containing new keys
+
+        Return:
+            Nothing
+        """
+        for each in json:
+            self.__dict__[each] = json[each]

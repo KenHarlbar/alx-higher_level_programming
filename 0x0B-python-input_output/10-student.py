1de27e853ef0e31a4cee99112c737f70deb30b15
baaioy <balotech514@gmail.com>
2022-07-06 03:22:01 +0100

    Define a student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Class module for Student """
+
+
+class Student:
+    """ Student class that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes Student
+
+        Args:
+            first_name: Student first name
+            last_name: Student last name
+            age: Student age
+
+        Returns:
+            nothing
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=''):
+        """ Function that retrieves a dictionary representation
+        of a Student instance
+
+        Returns:
+            dictionary representation of a Student Instance
+        """
+        dico = self.__dict__.copy()
+
+        if type(attr) == list:
+            for each in attr:
+                if type(each) != str:
+                    return dico
+
+            new_dict = {}
+
+            for each in attr:
+                try:
+                    new_dict[each] = dico[each]
+                except:
+                    continue
+            return new_dict
+
+        return dico

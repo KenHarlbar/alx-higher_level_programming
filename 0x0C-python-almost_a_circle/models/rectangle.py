135c14cedb35bed8d4c9a76b0197afb615c84ff2
baaioy <balotech514@gmail.com>
2022-07-08 17:24:03 +0100

    Display rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,7 +117,17 @@ class Rectangle(Base):
     def display(self):
         """ Method to display rectangle """
 
+        for y in range(self.__y):
+            print()
         for i in range(self.__height):
+            for x in range(self.__x):
+                print(' ', end='')
             for j in range(self.__width):
                 print('#', end='')
-            print('\n', end='')
+            print()
+
+    def __str__(self):
+        """ Method for overriding the __str__ method"""
+
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} \
+- {self.__width}/{self.__height}"

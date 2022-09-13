a2bc037891accface3af050fc5dba8666ed9549f
baaioy <balotech514@gmail.com>
2022-09-13 13:38:42 +0100

    Create City model in the database and print all cities in the database

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A script that prints all City objects from
+the database hbtn_0e_14_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import Base, City
+from model_state import State
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    session.close()
+    for state, city in st_cty:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+A python file that contains the class definition
+of a City and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_state import State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    A class that inherits from Base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

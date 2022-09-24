d514471cb2799f65c5052ed53362dbff1009b4e4
baaioy <balotech514@gmail.com>
2022-09-24 03:19:00 +0100

    Hot fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 
 with urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
-    print('Body response:$')
-    print(f'\t - type: {type(body)}$')
-    print(f'\t - content: {body}$')
-    print(f"\t - utf8 content: {body.decode('utf8')}$")
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+    print('\t- utf8 content: {}'.format(body.decode("utf-8")))
